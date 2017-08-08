04352c07f60333331fdc29cb943a28c666f67534
Hiroki Kobayashi <hiroki.kobayashi91768@gmail.com>
2017-08-08 15:33:24 -0700

    add separator js

--- js/gis.js
+++ js/gis.js
@@ -221,7 +221,7 @@ function showDetailPopUp(id) {
     $("#plant_tel").text(rowData[TEL]);
     $("#plant_river").text(rowData[RIVER]);
     $("#plant_impact").text(rowData[IMPACT]);
-    $("#plant_amount").text(rowData[AMOUNT] + "㎥/日");
+    $("#plant_amount").text((rowData[AMOUNT].replace(/^(-?\d+)(\d{3})/, "$1,$2")) + "㎥/日");
     // make chart
     makeChart(rowData);
     // show popup
